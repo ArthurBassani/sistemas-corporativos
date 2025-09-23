23e52e57db7cc4c0afdd746ca88a455a3ab12f55
josedev2003 <jose.dev.metz@gmail.com>
2025-09-23 15:04:47 -0300

    nest project creation

---
+++ candidato/src/health.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
+
+@Controller('health')
+export class HealthController {
+  constructor(private health: HealthCheckService) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      // Aqui você pode adicionar verificações específicas
+      // Por exemplo: database, redis, etc.
+    ]);
+  }
+}
