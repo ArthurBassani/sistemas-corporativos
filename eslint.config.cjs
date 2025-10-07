45e563fcc1fc7ea586032af7d96c94474575ca78
Arthur da Silva Bassani <arthursilvabassani@gmail.com>
2025-10-07 12:17:21 -0300

    fix(infra): arrumando configuração do package.json, eslint e prettier

---
+++ eslint.config.cjs
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    files: ["src/**/*.ts"],
+    ignores: ["dist", "node_modules"],
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
